8031cbafec28589a7210f2a9b69219d5f9069a5a
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-15 15:53:19 +0700

    student

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Student student1 = new Student(1, "M. Công", "Hà Nội", 30);
+
+            Student student2 = new Student();
+            student2.SetId(2);
+            student2.SetName("Ph. Quang Khánh");
+            student2.SetAddress("Lạng Sơn");
+            student2.SetAge(25);
+
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.Read();
+        }
+    }
+}
